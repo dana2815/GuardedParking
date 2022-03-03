b5f65a76d848f8610804e858ef008330a1b0b13a
uzytkownik <uzytkownik@kursGIT.pl>
2022-03-03 21:51:44 +0100

    init project

---
+++ guardedparking/src/main/java/com/quarded/parking/Main.java
@@ -0,0 +1,8 @@
+package com.quarded.parking;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("dziala");
+    }
+}
---
+++ guardedparking/src/test/java/com/quarded/parking/MainTest.java
@@ -0,0 +1,12 @@
+package com.quarded.parking;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        Main.main(null);
+    }
+
+}
